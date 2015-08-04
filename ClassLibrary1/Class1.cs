9cbada4b18d170dc5bd774fd1f928a2c817b0b0f
orlikb <orlik1212@gmail.com>
2015-08-04 16:16:47 +0200

    a
    a

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -1,11 +1,13 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
-namespace ClassLibrary1
+namespace libsarmata   
 {
-    public class Class1
+    public class path
     {
     }
 }
